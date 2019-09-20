45fddd7d255e3b773a7b2b2375811d39af4df87b
HuMart <manya01@protonmail.com>
2019-09-20 11:37:57 -0400

    pass ring-buffer need to change reverse

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,13 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
-
+    self.storage[self.current] = item
+    self.current +=1
+    if self.current == self.capacity:
+      self.current = 0
   def get(self):
-    pass
+    value = []
+    for i in self.storage:
+      if i:
+        value.append(i)
+    return value
